44780afe2383da6a0f6dec1df18574d9c420e3a4
HamzaSa1t <37mezo@gmail.com>
2025-07-27 19:13:01 +0300

    sunday, 22 remaining

---
+++ storage_backup/framework/views/699fb159eba6d8242811ff7718219201.php
@@ -0,0 +1,57 @@
+<?php if (isset($component)) { $__componentOriginalaa758e6a82983efcbf593f765e026bd9 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginalaa758e6a82983efcbf593f765e026bd9 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => $__env->getContainer()->make(Illuminate\View\Factory::class)->make('mail::message'),'data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('mail::message'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+# Your Multi-Factor Authentication (MFA) Code
+
+Hello <?php echo new \Illuminate\Support\EncodedHtmlString($userName ?? 'User'); ?>,
+
+Your Multi-Factor Authentication (MFA) code is:
+
+<?php if (isset($component)) { $__componentOriginal91214b38020aa1d764d4a21e693f703c = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal91214b38020aa1d764d4a21e693f703c = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => $__env->getContainer()->make(Illuminate\View\Factory::class)->make('mail::panel'),'data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('mail::panel'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+# <?php echo new \Illuminate\Support\EncodedHtmlString($otpCode); ?>
+
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal91214b38020aa1d764d4a21e693f703c)): ?>
+<?php $attributes = $__attributesOriginal91214b38020aa1d764d4a21e693f703c; ?>
+<?php unset($__attributesOriginal91214b38020aa1d764d4a21e693f703c); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal91214b38020aa1d764d4a21e693f703c)): ?>
+<?php $component = $__componentOriginal91214b38020aa1d764d4a21e693f703c; ?>
+<?php unset($__componentOriginal91214b38020aa1d764d4a21e693f703c); ?>
+<?php endif; ?>
+
+This code is valid for <?php echo new \Illuminate\Support\EncodedHtmlString(config('auth.mfa_otp_expiration', 5)); ?> minutes. Please do not share this code with anyone.
+
+If you did not request this code, please ignore this email.
+
+Thanks,<br>
+<?php echo new \Illuminate\Support\EncodedHtmlString(config('app.name')); ?>
+
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginalaa758e6a82983efcbf593f765e026bd9)): ?>
+<?php $attributes = $__attributesOriginalaa758e6a82983efcbf593f765e026bd9; ?>
+<?php unset($__attributesOriginalaa758e6a82983efcbf593f765e026bd9); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalaa758e6a82983efcbf593f765e026bd9)): ?>
+<?php $component = $__componentOriginalaa758e6a82983efcbf593f765e026bd9; ?>
+<?php unset($__componentOriginalaa758e6a82983efcbf593f765e026bd9); ?>
+<?php endif; ?>
+<?php /**PATH C:\cursor1\laravel-app\resources\views/emails/mfa/otp.blade.php ENDPATH**/ ?>
---
+++ storage_backup/framework/views/e0e868b529c539c22139806ec3e43c29.php
@@ -0,0 +1,14 @@
+<?php echo strip_tags($header ?? ''); ?>
+
+
+<?php echo strip_tags($slot); ?>
+
+<?php if(isset($subcopy)): ?>
+
+<?php echo strip_tags($subcopy); ?>
+
+<?php endif; ?>
+
+<?php echo strip_tags($footer ?? ''); ?>
+
+<?php /**PATH C:\cursor1\laravel-app\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/layout.blade.php ENDPATH**/ ?>
